92bc74a4410e36a63f1cdb801754880ad4cdf795
v-lungu <v_lungu@hotmail.com>
2023-10-03 16:51:14 -0400

    Enabled cross origin capabilities

--- src/main/java/com/vlungu/backend/birthday/controller/BirthdayController.java
+++ src/main/java/com/vlungu/backend/birthday/controller/BirthdayController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class BirthdayController {
